fff0ad12aec82276bf710a33b1371a84dbb8cfcd
bb <bb@codepoet.de>
2021-01-02 21:20:09 +0100

    Add missing changes to jest-config/preact

--- jest-config/preact/jest-preset.js
+++ jest-config/preact/jest-preset.js
@@ -1,30 +1,30 @@
 module.exports = {
-  "moduleNameMapper": {
-    "\\.(css)$": "identity-obj-proxy",
-    "^react$": "preact/compat",
-    "^react-dom$": "preact/compat",
-    "^react-dom/test-utils$": "preact/test-utils"
-  },
-  "moduleDirectories": [
-    "node_modules",
-    "src"
-  ],
-  "transform": {
-    "\\.(tsx?)$": "babel-jest",
-  },
-  "setupFilesAfterEnv": [
-    "@testing-library/jest-dom",
-    "jest-axe/extend-expect",
-    "@benbender/jest-config/jest-setup.js",
-  ],
-  "testPathIgnorePatterns": [
-    "/dist/",
-    "/.next/",
-    "/out/",
-    "/node_modules/"
-  ],
-  "watchPlugins": [
-    "jest-watch-typeahead/filename",
-    "jest-watch-typeahead/testname"
-  ]
+    "moduleNameMapper": {
+        "\\.(css)$": "identity-obj-proxy",
+        "^react$": "preact/compat",
+        "^react-dom$": "preact/compat",
+        "^react-dom/test-utils$": "preact/test-utils"
+    },
+    "moduleDirectories": [
+        "node_modules",
+        "src"
+    ],
+    "transform": {
+        "\\.(tsx?)$": "babel-jest",
+    },
+    "setupFilesAfterEnv": [
+        "@testing-library/jest-dom",
+        "jest-axe/extend-expect",
+        "@benbender/jest-config/jest-setup.js",
+    ],
+    "testPathIgnorePatterns": [
+        "/dist/",
+        "/.next/",
+        "/out/",
+        "/node_modules/"
+    ],
+    "watchPlugins": [
+        "jest-watch-typeahead/filename",
+        "jest-watch-typeahead/testname"
+    ]
 }

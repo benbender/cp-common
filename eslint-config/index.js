ecd2e3411e2789d25f3b525e986ce50ce436dcdb
bb <bb@codepoet.de>
2021-01-03 11:24:46 +0100

    Adds several overrides to eslint-config to respect nextjs

--- eslint-config/index.js
+++ eslint-config/index.js
@@ -31,14 +31,17 @@ module.exports = {
     ],
     extends: [
         "eslint:recommended",
+        "prettier",
+        "prettier/react",
+        "prettier/prettier",
+        "prettier/@typescript-eslint",
         "plugin:@typescript-eslint/recommended",
         "plugin:import/errors",
         "plugin:import/warnings",
         "plugin:import/typescript",
         "plugin:prettier/recommended",
         "plugin:promise/recommended",
-        "prettier/@typescript-eslint",
-        "prettier/react",
+        "plugin:@next/next/recommended",
         "plugin:react/recommended",
         "plugin:react-hooks/recommended",
         "plugin:jest-dom/recommended",
@@ -50,5 +53,36 @@ module.exports = {
         "react/prop-types": "off", // Ignore prop-types as we are using typescript
         "react/react-in-jsx-scope": "off", // React is always in scope with Next.js
         "jsx-a11y/anchor-is-valid": "off", // Doesn't play well with next/link
+        "import/no-anonymous-default-export": "error", // For named React components
     },
+    "overrides": [
+        {
+            "files": ["*.js"],
+            "rules": {
+                "@typescript-eslint/explicit-module-boundary-types": "off",
+                "@typescript-eslint/no-var-requires": "off", // Allow CommonJS imports
+                "global-require": "off" // Allow dynamic imports for Node
+            }
+        },
+        {
+            "files": ["*.tsx"],
+            "rules": {
+                "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+                "react/jsx-props-no-spreading": "off" // TypeScript makes this safe
+            }
+        },
+        {
+            "files": ["./src/pages/**"],
+            "rules": {
+                "import/no-default-export": "off",
+                "import/prefer-default-export": "error"
+            }
+        },
+        {
+            "files": ["./src/pages/api/**"],
+            "rules": {
+                "import/no-anonymous-default-export": "off"
+            }
+        }
+    ]
 };
